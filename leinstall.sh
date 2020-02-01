665ce3bf427a04e8b90a833210b0c16a4cb17855
Olexiy Buyanskyy <olexiy.b@blaize.tech>
2020-02-01 19:04:49 +0200

    added support of let's encrypt certificate

--- leinstall.sh
+++ leinstall.sh
@@ -2,10 +2,9 @@
 set -x
 set -o
 
-cd /root
+cd /var/lib/ipa-data/root
 wget https://letsencrypt.org/certs/isrgrootx1.pem
 wget https://letsencrypt.org/certs/letsencryptauthorityx3.pem
-ipa-cacert-manage install isrgrootx1.pem -n ISRGRootCAX1 -t C,,
-ipa-cacert-manage install letsencryptauthorityx3.pem -n ISRGRootCAX3 -t C,,
-kinit admin
-ipa-certupdate
+docker exec freeipa-server ipa-cacert-manage install /data/root/isrgrootx1.pem -n ISRGRootCAX1 -t C,,
+docker exec freeipa-server ipa-cacert-manage install /data/root/letsencryptauthorityx3.pem -n ISRGRootCAX3 -t C,,
+docker exec -ti freeipa-server bash -c "kinit admin && ipa-certupdate"
